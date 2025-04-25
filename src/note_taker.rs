26e227490ef01421c0dfba5fea73fefbb0811f0a
akira-erick <kajitani.motta@gmail.com>
2025-04-25 10:28:18 -0300

    test: Add test: 'test_should_return_amount_of_notes'

--- src/note_taker.rs
+++ src/note_taker.rs
@@ -107,4 +107,15 @@ mod tests {
         assert_eq!(note_taker.get_note(0), note2);
         assert_eq!(note_taker.get_note(1), note1);
     }
+
+    fn test_should_return_amount_of_notes() {
+        let mut note_taker = NoteTaker::new();
+        let note1 = Note::new("Title 1".to_string(), "Content 1".to_string());
+        let note2 = Note::new("Title 2".to_string(), "Content 2".to_string());
+
+        note_taker.add_note(note1.clone());
+        note_taker.add_note(note2.clone());
+
+        assert_eq!(note_taker.get_size(), 2);
+    }
 }
