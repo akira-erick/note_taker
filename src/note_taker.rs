5e4e0f093d6523857b276d09c5be1555a9be328d
akira-erick <kajitani.motta@gmail.com>
2025-04-24 22:19:10 -0300

    test: Add test: 'test_should_not_allow_duplicate_notes'

--- src/note_taker.rs
+++ src/note_taker.rs
@@ -35,4 +35,16 @@ mod tests {
         assert_eq!(note_taker.get_notes()[0], note1);
         assert_eq!(note_taker.get_notes()[1], note2);
     }
+
+    #[test]
+    fn test_should_not_allow_duplicate_notes() {
+        let mut note_taker = NoteTaker::new();
+        let note1 = Note::new("Title 1".to_string(), "Content 1".to_string());
+
+        note_taker.add_note(note1.clone());
+        note_taker.add_note(note1.clone());
+
+        assert_eq!(note_taker.get_notes().len(), 1);
+        assert_eq!(note_taker.get_notes()[0], note1);
+    }
 }
